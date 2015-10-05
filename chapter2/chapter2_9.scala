5cc13fcd0a5d75cb4ee2f20eef81816fca7c5609
Aniket Joshi <aniket.joshi@nike.com>
2015-10-05 13:58:06 -0700

    refactored code for problem 9

--- chapter2/chapter2_9.scala
+++ chapter2/chapter2_9.scala
@@ -3,11 +3,16 @@ object problem9 {
 
 def productRecursive(str:String):Int = {
   @tailrec
-  def productAccumulator(str:Array[Char], accum:Int): Int = {
-    if(str.length == 0) 1
-    else productAccumulator(str, accum * str.map(_.toInt))
-  }
-  productAccumulator(str.toCharArray, 1)
+  def productAccumulator(accum:Int, idx:Int): Int = {
+    if(str.length == idx) accum  
+    else {
+        val currentChar = str(idx).toInt
+        val updatedAccum = accum * currentChar
+        val nextIdx = idx + 1
+        productAccumulator(updatedAccum, nextIdx)
+   }  
+ }
+  productAccumulator(1, 0)
  } 
  def main(args:Array[String]): Unit = {
    println("Recusive product:"+productRecursive(args(0)))  
